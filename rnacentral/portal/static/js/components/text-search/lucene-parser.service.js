6437a79ccf50fcc6662817d51458b0284ee95d96
Boris A. Burkov <burkov@ebi.ac.uk>
2018-03-04 20:52:13 +0000

    #132 Numerous bugfixes in unparse().

--- rnacentral/portal/static/js/components/text-search/lucene-parser.service.js
+++ rnacentral/portal/static/js/components/text-search/lucene-parser.service.js
@@ -70,16 +70,17 @@ var luceneParser = function() {
 
         var expression; // expression is the currently evaluated
         while (stack.length > 0) {
-            expression = stack.pop();
+            // we're using shift/unshift instead of push/pop in order to avoid reversing arguments order
+            expression = stack.shift();
 
             if (typeof expression == 'string') {
                 result = result + expression;
             } else if (this._type(expression) === this.TYPES.NODE) {
-                stack.push('(', expression.left, ' ', expression.operator, ' ', expression.right, ')');
+                stack.unshift('(', expression.left, ' ', expression.operator, ' ', expression.right, ')');
             } else if (this._type(expression) === this.TYPES.FIELD) {
                 var prefix = expression.prefix ? expression.prefix : '';
-                if (expression.field !== '<implicit>') substring += prefix + expression.term;
-                else substring += expression.field + ':' + prefix + expression.term
+                if (expression.field === '<implicit>') result += prefix + expression.term;
+                else result += expression.field + ':' + prefix + expression.term
             } else if (this._type(expression) === this.TYPES.RANGE) {
                 var inclusive_min = expression.inclusive_min ? expression.inclusive_min : expression.inclusive;
                 var inclusive_max = expression.inclusive_max ? expression.inclusive_max : expression.inclusive;
@@ -87,7 +88,7 @@ var luceneParser = function() {
                 var delimiter_max = inclusive_max ? ']' : '}';
 
                 // expression.field has to be defined - we don't accept ranges without field name
-                substring += expression.field + ":" + delimiter_min + expression.term_min + " TO " + expression.term_max + delimiter_max;
+                result += expression.field + ":" + delimiter_min + expression.term_min + " TO " + expression.term_max + delimiter_max;
             } else {
                 throw "AST expression of unknown type: " + expression;
             }
@@ -104,9 +105,10 @@ var luceneParser = function() {
      * @private
      */
     this._type = function(expression) {
-        if (expression.hasOwnProperty('left')) return 'node';
-        else if (expression.hasOwnProperty('term')) return 'field';
-        else return 'range';
+        if (expression.hasOwnProperty('left')) return this.TYPES.NODE;
+        else if (expression.hasOwnProperty('term')) return this.TYPES.FIELD;
+        else if (expression.hasOwnProperty('term_min')) return this.TYPES.RANGE;
+        else throw "AST expression of unknown type: " + JSON.stringify(expression);
     };
 
     /**
